b314c13abe9f1d8996dcc1a62e1a9ce8005df0ae
Guillaume Grossetie <ggrossetie@gmail.com>
2018-10-14 10:09:18 +0200

    Upgrade to Node.js 8+
    - Use Promise for async functions
    - Use standard.js to format code
    - Update README
    - Update LICENSE (copyright 2018)
    - Build against the latest stable and LTS Node.js releases on Travis
    - Update dependencies

--- index.js
+++ index.js
@@ -1,133 +1,133 @@
-var fs = require('fs');
-var zlib = require('zlib');
-var tar = require('tar-fs');
-var path = require('path');
-var log = require('bestikk-log');
+const fs = require('fs')
+const zlib = require('zlib')
+const tar = require('tar-fs')
+const path = require('path')
+const log = require('bestikk-log')
 
-var FS = function() {
+const FS = function () {
 }
 
-FS.prototype.removeSync = function(dir) {
-  var files = [];
-  var bfs = this;
+FS.prototype.removeSync = function (dir) {
+  let files = []
+  const bfs = this
   if (fs.existsSync(dir)) {
-    files = fs.readdirSync(dir);
-    files.forEach(function(file){
-      var curPath = dir + "/" + file;
+    files = fs.readdirSync(dir)
+    files.forEach(function (file) {
+      var curPath = dir + '/' + file
       if (fs.lstatSync(curPath).isDirectory()) { // recurse
-        bfs.removeSync(curPath);
+        bfs.removeSync(curPath)
       } else { // delete file
-        fs.unlinkSync(curPath);
+        fs.unlinkSync(curPath)
       }
-    });
-    fs.rmdirSync(dir);
+    })
+    fs.rmdirSync(dir)
   }
-};
+}
 
 // https://github.com/jprichardson/node-fs-extra/blob/master/lib/mkdirs/mkdirs-sync.js
-FS.prototype.mkdirsSync = function(p, made) {
-  p = path.resolve(p);
+FS.prototype.mkdirsSync = function (p, made) {
+  p = path.resolve(p)
   try {
-    fs.mkdirSync(p);
-    made = made || p;
+    fs.mkdirSync(p)
+    made = made || p
   } catch (err0) {
     switch (err0.code) {
       case 'ENOENT' :
-        made = this.mkdirsSync(path.dirname(p), made);
-        this.mkdirsSync(p, made);
-        break;
+        made = this.mkdirsSync(path.dirname(p), made)
+        this.mkdirsSync(p, made)
+        break
 
       // In the case of any other error, just see if there's a dir
       // there already.  If so, then hooray!  If not, then something
       // is borked.
       default:
-        var stat;
+        let stat
         try {
-          stat = fs.statSync(p);
+          stat = fs.statSync(p)
         } catch (err1) {
-          throw err0;
+          throw err0
         }
-        if (!stat.isDirectory()) throw err0;
-        break;
+        if (!stat.isDirectory()) throw err0
+        break
     }
   }
-  return made;
+  return made
 }
 
-FS.prototype.copySync = function(src, dest) {
-  var bfs = this;
-  var exists = fs.existsSync(src);
-  var stats = exists && fs.statSync(src);
-  var isDirectory = exists && stats.isDirectory();
+FS.prototype.copySync = function (src, dest) {
+  const bfs = this
+  const exists = fs.existsSync(src)
+  const stats = exists && fs.statSync(src)
+  const isDirectory = exists && stats.isDirectory()
   if (exists && isDirectory) {
-    fs.readdirSync(src).forEach(function(childItemName) {
-      bfs.copySync(path.join(src, childItemName), path.join(dest, childItemName));
-    });
+    fs.readdirSync(src).forEach(function (childItemName) {
+      bfs.copySync(path.join(src, childItemName), path.join(dest, childItemName))
+    })
   } else {
-    this.mkdirsSync(path.dirname(dest));
-    var data = fs.readFileSync(src);
-    log.transform('copy', src, dest);
-    fs.writeFileSync(dest, data);
+    this.mkdirsSync(path.dirname(dest))
+    const data = fs.readFileSync(src)
+    log.transform('copy', src, dest)
+    fs.writeFileSync(dest, data)
   }
-};
+}
 
-FS.prototype.walk = function(dir, callback) {
-  var bfs = this;
-  fs.readdirSync(dir).forEach(function(name) {
-    var filePath = path.join(dir, name);
-    var stat = fs.statSync(filePath);
+FS.prototype.walk = function (dir, action) {
+  const bfs = this
+  fs.readdirSync(dir).forEach(function (name) {
+    const filePath = path.join(dir, name)
+    const stat = fs.statSync(filePath)
     if (stat.isFile()) {
-      callback(filePath, stat);
+      action(filePath, stat)
     } else if (stat.isDirectory()) {
-      bfs.walk(filePath, callback);
+      bfs.walk(filePath, action)
     }
-  });
-};
+  })
+}
 
-FS.prototype.untar = function(source, baseDirName, destinationDir, callback) {
-  log.transform('untar', source, destinationDir + '/' + baseDirName); 
-  var stream = fs.createReadStream(source).pipe(zlib.createGunzip()).pipe(tar.extract(destinationDir, {
+FS.prototype.untar = function (source, baseDirName, destinationDir) {
+  log.transform('untar', source, destinationDir + '/' + baseDirName)
+  const stream = fs.createReadStream(source).pipe(zlib.createGunzip()).pipe(tar.extract(destinationDir, {
     map: function (header) {
       // REMIND Do NOT user path.sep!
       // In this case, even on Windows, the separator is '/'.
-      var paths = header.name.split('/');
+      var paths = header.name.split('/')
       // replace base directory with 'baseDirName'
-      paths.shift();
-      paths.unshift(baseDirName);
-      header.name = paths.join('/');
-      return header;
+      paths.shift()
+      paths.unshift(baseDirName)
+      header.name = paths.join('/')
+      return header
     }
-  }));
-  stream.on('finish', function () {
-    callback();
-  });
-};
+  }))
+  return new Promise((resolve, reject) => {
+    stream.on('finish', resolve)
+    stream.on('error', reject)
+  })
+}
 
-FS.prototype.concatSync = function(files, destination) {
-  var fs = require('fs');
-  var buffers = [];
-  var filesLength = files.length;
+FS.prototype.concatSync = function (files, destination) {
+  const buffers = []
+  const filesLength = files.length
   for (var i = 0; i < filesLength; i++) {
-    var buffer = fs.readFileSync(files[i]);
-    if (i == (files.length - 1)) {
-      buffers.push(buffer);
+    const buffer = fs.readFileSync(files[i])
+    if (i === (files.length - 1)) {
+      buffers.push(buffer)
     } else {
-      buffers.push(Buffer.concat([buffer, new Buffer('\n')]));
+      buffers.push(Buffer.concat([buffer, Buffer.from('\n')]))
     }
   }
-  fs.writeFileSync(destination, Buffer.concat(buffers));
-};
+  fs.writeFileSync(destination, Buffer.concat(buffers))
+}
 
-FS.prototype.copyToDirSync = function(from, toDir) {
-  var basename = path.basename(from);
-  this.copySync(from, path.join(toDir, basename));
-};
+FS.prototype.copyToDirSync = function (from, toDir) {
+  const basename = path.basename(from)
+  this.copySync(from, path.join(toDir, basename))
+}
 
-FS.prototype.updateFileSync = function(file, regexp, newSubString) {
-  log.debug('update ' + file);
-  var data = fs.readFileSync(file, 'utf8');
-  var dataUpdated = data.replace(regexp, newSubString);
-  fs.writeFileSync(file, dataUpdated, 'utf8');
-};
+FS.prototype.updateFileSync = function (file, regexp, newSubString) {
+  log.debug('update ' + file)
+  const data = fs.readFileSync(file, 'utf8')
+  const dataUpdated = data.replace(regexp, newSubString)
+  fs.writeFileSync(file, dataUpdated, 'utf8')
+}
 
-module.exports = new FS();
+module.exports = new FS()
--- spec/spec.js
+++ spec/spec.js
@@ -1,183 +1,182 @@
-var fs = require('fs');
-var path = require('path');
-var os = require('os');
-var chai = require('chai');
-var assert = chai.assert;
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+const chai = require('chai')
+const assert = chai.assert
 
-var main = require('../index.js');
+const main = require('../index.js')
 
-describe('Bestikk', function() {
-  var TEST_DIR;
+describe('Bestikk', function () {
+  let TEST_DIR
 
   beforeEach(function () {
-    TEST_DIR = path.join(os.tmpdir(), 'test', 'mkdir');
-    main.removeSync(TEST_DIR);
-  });
+    TEST_DIR = path.join(os.tmpdir(), 'test', 'mkdir')
+    main.removeSync(TEST_DIR)
+  })
 
   afterEach(function () {
-    main.removeSync(TEST_DIR);
-  });
+    main.removeSync(TEST_DIR)
+  })
 
   describe('+ copySync()', function () {
     it('should copy the directory and all its content', function (done) {
-      var src = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
-      var dest = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      const src = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
+      const dest = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
 
-      assert(!fs.existsSync(src));
-      assert(!fs.existsSync(dest));
-      main.mkdirsSync(src);
-      assert(fs.existsSync(src));
+      assert(!fs.existsSync(src))
+      assert(!fs.existsSync(dest))
+      main.mkdirsSync(src)
+      assert(fs.existsSync(src))
       // src/foo
-      fs.openSync(path.join(src, 'foo'), 'w');
+      fs.openSync(path.join(src, 'foo'), 'w')
       // src/bar/baz
-      main.mkdirsSync(path.join(src, 'bar'));
-      fs.openSync(path.join(src, 'bar', 'baz'), 'w');
-      main.copySync(src, dest);
-      assert(fs.existsSync(dest));
-      assert(fs.existsSync(path.join(dest, 'foo')));
-      assert(fs.existsSync(path.join(dest, 'bar')));
-      assert(fs.existsSync(path.join(dest, 'bar', 'baz')));
+      main.mkdirsSync(path.join(src, 'bar'))
+      fs.openSync(path.join(src, 'bar', 'baz'), 'w')
+      main.copySync(src, dest)
+      assert(fs.existsSync(dest))
+      assert(fs.existsSync(path.join(dest, 'foo')))
+      assert(fs.existsSync(path.join(dest, 'bar')))
+      assert(fs.existsSync(path.join(dest, 'bar', 'baz')))
 
-      done();
-    });
-  });
+      done()
+    })
+  })
 
   describe('+ mkdirsSync()', function () {
     it('should make the directory', function (done) {
-      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      const dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
 
-      assert(!fs.existsSync(dir));
-      main.mkdirsSync(dir);
-      assert(fs.existsSync(dir));
+      assert(!fs.existsSync(dir))
+      main.mkdirsSync(dir)
+      assert(fs.existsSync(dir))
 
-      done();
-    });
+      done()
+    })
 
     it('should make the entire directory path', function (done) {
-      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
-      var newDir = path.join(dir, 'dfdf', 'ffff', 'aaa');
+      const dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
+      const newDir = path.join(dir, 'dfdf', 'ffff', 'aaa')
 
-      assert(!fs.existsSync(newDir));
-      main.mkdirsSync(newDir);
-      assert(fs.existsSync(newDir));
+      assert(!fs.existsSync(newDir))
+      main.mkdirsSync(newDir)
+      assert(fs.existsSync(newDir))
 
-      done();
-    });
-  });
+      done()
+    })
+  })
 
   describe('+ removeSync()', function () {
     it('should remove the directory and all its content', function (done) {
-      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      const dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
 
-      assert(!fs.existsSync(dir));
-      main.mkdirsSync(dir);
-      assert(fs.existsSync(dir));
-      fs.openSync(path.join(dir, 'foo'), 'w');
-      fs.openSync(path.join(dir, 'bar'), 'w');
-      fs.openSync(path.join(dir, 'baz'), 'w');
-      main.removeSync(dir);
-      assert(!fs.existsSync(dir));
+      assert(!fs.existsSync(dir))
+      main.mkdirsSync(dir)
+      assert(fs.existsSync(dir))
+      fs.openSync(path.join(dir, 'foo'), 'w')
+      fs.openSync(path.join(dir, 'bar'), 'w')
+      fs.openSync(path.join(dir, 'baz'), 'w')
+      main.removeSync(dir)
+      assert(!fs.existsSync(dir))
 
-      done();
-    });
-  });
+      done()
+    })
+  })
 
   describe('+ untar()', function () {
     it('should untar the tar file', function (done) {
-      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
-
-      assert(!fs.existsSync(dir));
-      main.mkdirsSync(dir);
-      assert(fs.existsSync(dir));
-      main.copySync(path.join(__dirname, 'samples', 'foo.tar.gz'), path.join(dir, 'foo.tar.gz'));
-      main.untar(path.join(dir, 'foo.tar.gz'), 'qux', dir, function() {
-        assert(fs.existsSync(path.join(dir, 'qux')));
-        assert(fs.existsSync(path.join(dir, 'qux', 'bar')));
-        assert(fs.existsSync(path.join(dir, 'qux', 'baz')));
-        done();
-      });
-    });
-  });
+      const dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
+      assert(!fs.existsSync(dir))
+      main.mkdirsSync(dir)
+      assert(fs.existsSync(dir))
+      main.copySync(path.join(__dirname, 'samples', 'foo.tar.gz'), path.join(dir, 'foo.tar.gz'))
+      main.untar(path.join(dir, 'foo.tar.gz'), 'qux', dir)
+        .then(() => {
+          assert(fs.existsSync(path.join(dir, 'qux')))
+          assert(fs.existsSync(path.join(dir, 'qux', 'bar')))
+          assert(fs.existsSync(path.join(dir, 'qux', 'baz')))
+          done()
+        })
+    })
+  })
 
   describe('+ walk()', function () {
     it('should walk through the directory', function (done) {
-      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      const dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
 
-      assert(!fs.existsSync(dir));
-      main.mkdirsSync(dir);
-      assert(fs.existsSync(dir));
+      assert(!fs.existsSync(dir))
+      main.mkdirsSync(dir)
+      assert(fs.existsSync(dir))
       // dir/foo
-      fs.openSync(path.join(dir, 'foo'), 'w');
+      fs.openSync(path.join(dir, 'foo'), 'w')
       // dir/bar/baz
-      main.mkdirsSync(path.join(dir, 'bar'));
-      fs.openSync(path.join(dir, 'bar', 'baz'), 'w');
-      var paths = [];
-      var stats = [];
-      main.walk(dir, function(path, stat) {
-        paths.push(path);
-        stats.push(stat);
-      });
-      assert.equal(paths.length, 2);
-      assert.equal(paths[0], path.join(dir, 'bar', 'baz'));
-      assert.equal(paths[1], path.join(dir, 'foo'));
-      assert.equal(stats.length, 2);
-      assert(stats[0].isFile());
-      assert(stats[1].isFile());
+      main.mkdirsSync(path.join(dir, 'bar'))
+      fs.openSync(path.join(dir, 'bar', 'baz'), 'w')
+      const paths = []
+      const stats = []
+      main.walk(dir, function (path, stat) {
+        paths.push(path)
+        stats.push(stat)
+      })
+      assert.equal(paths.length, 2)
+      assert.equal(paths[0], path.join(dir, 'bar', 'baz'))
+      assert.equal(paths[1], path.join(dir, 'foo'))
+      assert.equal(stats.length, 2)
+      assert(stats[0].isFile())
+      assert(stats[1].isFile())
 
-      done();
-    });
-  });
+      done()
+    })
+  })
 
   describe('+ concatSync()', function () {
     it('should concat files into the destination file', function (done) {
-      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      const dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
 
-      assert(!fs.existsSync(dir));
-      main.mkdirsSync(dir);
-      fs.writeFileSync(path.join(dir, 'foo'), 'foo', {encoding: 'utf8'});
-      fs.writeFileSync(path.join(dir, 'bar'), 'bar', {encoding: 'utf8'});
-      fs.writeFileSync(path.join(dir, 'baz'), 'baz', {encoding: 'utf8'});
-      main.concatSync([path.join(dir, 'foo'), path.join(dir, 'bar'), path.join(dir, 'baz')], path.join(dir, 'qux'));
+      assert(!fs.existsSync(dir))
+      main.mkdirsSync(dir)
+      fs.writeFileSync(path.join(dir, 'foo'), 'foo', { encoding: 'utf8' })
+      fs.writeFileSync(path.join(dir, 'bar'), 'bar', { encoding: 'utf8' })
+      fs.writeFileSync(path.join(dir, 'baz'), 'baz', { encoding: 'utf8' })
+      main.concatSync([path.join(dir, 'foo'), path.join(dir, 'bar'), path.join(dir, 'baz')], path.join(dir, 'qux'))
 
-      assert(fs.existsSync(path.join(dir, 'qux')));
-      assert.equal(fs.readFileSync(path.join(dir, 'qux'), {encoding: 'utf8'}), 'foo\nbar\nbaz');
+      assert(fs.existsSync(path.join(dir, 'qux')))
+      assert.equal(fs.readFileSync(path.join(dir, 'qux'), { encoding: 'utf8' }), 'foo\nbar\nbaz')
 
-      done();
-    });
-  });
+      done()
+    })
+  })
 
   describe('+ copyToDirSync()', function () {
     it('should copy the file to the directory', function (done) {
-      var src = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
-      var dest = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      const src = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
+      const dest = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
 
-      assert(!fs.existsSync(src));
-      assert(!fs.existsSync(dest));
-      main.mkdirsSync(src);
-      assert(fs.existsSync(src));
+      assert(!fs.existsSync(src))
+      assert(!fs.existsSync(dest))
+      main.mkdirsSync(src)
+      assert(fs.existsSync(src))
       // src/foo
-      fs.openSync(path.join(src, 'foo'), 'w');
-      main.copyToDirSync(path.join(src, 'foo'), dest);
-      assert(fs.existsSync(dest));
-      assert(fs.existsSync(path.join(dest, 'foo')));
+      fs.openSync(path.join(src, 'foo'), 'w')
+      main.copyToDirSync(path.join(src, 'foo'), dest)
+      assert(fs.existsSync(dest))
+      assert(fs.existsSync(path.join(dest, 'foo')))
 
-      done();
-    });
-  });
+      done()
+    })
+  })
 
   describe('+ updateFileSync()', function () {
     it('should update the version in package.json with 1.0.0', function (done) {
-      var dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      const dir = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random())
 
-      assert(!fs.existsSync(dir));
-      main.mkdirsSync(dir);
-      fs.writeFileSync(path.join(dir, 'package.json'), '{"version": "0.1.0"}', {encoding: 'utf8'});
-      main.updateFileSync(path.join(dir, 'package.json'), /"version": "(.*?)"/g, '"version": "1.0.0"');
+      assert(!fs.existsSync(dir))
+      main.mkdirsSync(dir)
+      fs.writeFileSync(path.join(dir, 'package.json'), '{"version": "0.1.0"}', { encoding: 'utf8' })
+      main.updateFileSync(path.join(dir, 'package.json'), /"version": "(.*?)"/g, '"version": "1.0.0"')
 
-      assert.equal(fs.readFileSync(path.join(dir, 'package.json'), {encoding: 'utf8'}), '{"version": "1.0.0"}');
+      assert.equal(fs.readFileSync(path.join(dir, 'package.json'), { encoding: 'utf8' }), '{"version": "1.0.0"}')
 
-      done();
-    });
-  });
-
-});
+      done()
+    })
+  })
+})
